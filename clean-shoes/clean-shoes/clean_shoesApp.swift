3388cbae4591a934026133e7c921f4c066f4f19d
Dimitar Prisadnikov <i454903@fhict.nl>
2022-04-04 16:06:00 +0200

    project creation

---
+++ clean-shoes/clean-shoes/clean_shoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  clean_shoesApp.swift
+//  clean-shoes
+//
+//  Created by Dimitar Prisadnikov on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct clean_shoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
