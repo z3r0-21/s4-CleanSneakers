af6e98cf9db53565cf51abb71d8c60db960dad62
Dimitar Prisadnikov <i454903@fhict.nl>
2022-04-06 11:51:38 +0200

    added weather codes and switch statement for different messages for each of those codes (the messages content will be added later)

--- clean-shoes/WeatherAPIClient/WeatherModel.swift
+++ clean-shoes/WeatherAPIClient/WeatherModel.swift
@@ -27,3 +27,59 @@ struct WeatherIntervals: Codable {
 struct WeatherValue: Codable {
     var weatherCode: Int
 }
+
+
+enum WeatherCode: String, Codable {
+    //Clear
+    case clear = "1000"
+    case mostlyClear = "1100"
+    
+    //Cloudy or/and fog
+    case partlyCloudy = "1101"
+    case mostlyCloudy = "1102"
+    case cloudy = "1001"
+    case fog = "2000"
+    case lightFog = "2100"
+    
+    //Wind
+    case lightWind = "3000"
+    case wind = "3001"
+    case strongWind = "3002"
+    
+    //Rain
+    case drizzle = "4000"
+    case rain = "4001"
+    case lightRain = "4200"
+    case heavyRain = "4201"
+    case thunderstorm = "8000"
+    
+    //Snow or/and ice
+    case snow = "5000"
+    case flurries = "5001"
+    case lightSnow = "5100"
+    case heavySnow = "5101"
+    case freezingDrizzle = "6000"
+    case freezingRain = "6001"
+    case lightFreezingRain = "6200"
+    case heavyFreezingRain = "6201"
+    case icePellets = "7000"
+    case heavyIcePellets = "7101"
+    case lightIcePellets = "7102"
+    
+    //todo: update with relevant messages
+    var description: String {
+        switch self {
+        case .clear, .mostlyClear:
+            return ""
+        case .partlyCloudy, .mostlyCloudy, .cloudy, .fog, .lightFog:
+            return ""
+        case .lightWind, .wind, .strongWind:
+            return ""
+        case .drizzle, .rain, .lightRain, .heavyRain, .thunderstorm:
+            return ""
+        case .snow, .flurries, .lightSnow, .heavySnow, .freezingDrizzle, .freezingRain, .lightFreezingRain, .heavyFreezingRain, .icePellets, .heavyIcePellets, .lightIcePellets:
+            return ""
+
+        }
+    }
+}
