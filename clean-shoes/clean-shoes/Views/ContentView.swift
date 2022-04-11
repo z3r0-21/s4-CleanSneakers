168a0b9ace0f0002b01fc42c6a405cd5cb6eb6eb
Dimitar Prisadnikov <i454903@fhict.nl>
2022-04-11 15:51:49 +0200

    added main navigation bar

--- clean-shoes/clean-shoes/Views/ContentView.swift
+++ clean-shoes/clean-shoes/Views/ContentView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .today
+
+    enum Tab {
+        case today
+        case forecast
+        case sneakers
+    }
+
     var body: some View {
-        Text("Hey")
+        TabView(selection: $selection) {
+            Text("today")
+                .tabItem {
+                    Label("Today", systemImage: "sun.min")
+                }
+                .tag(Tab.today)
+
+            Text("forecast")
+                .tabItem {
+                    Label("Forecast", systemImage: "calendar")
+                }
+                .tag(Tab.forecast)
+            
+            Text("sneakers")
+                .tabItem {
+                    Label("Sneakers", systemImage: "list.bullet")
+                }
+                .tag(Tab.sneakers)
+        }
     }
 }
 
